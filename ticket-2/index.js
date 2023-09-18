7591aa7fd4230c82b9d4700e74cf6053fb4b5e12
MartiZu <martina.zurli@gmail.com>
2023-09-18 15:41:25 +0100

    ticket2

--- ticket-2/index.js
+++ ticket-2/index.js
@@ -1,13 +1,29 @@
-Functin extractInitialsFromFullName(fullName) {
+function extractInitialsFromFullName(fullName) {
 /** Assumes that each name within the full name is separated by a "space" that can be used as a separator/delimiter.  */
-const         names            = fullName.split(" ");
+const names = fullName.split(" ");
+// .split is used to split a string into an array of individual characters
+//names is an array where each element represents a single character from the original string
 let initials = "";
 
-For (let i = 0; i < names.length; i++) [
-Const currentName = names[i];
-const currentInitial = currentName.at(0);
-initials += currentInitial;
+    for (let i = 0; i < names.length; i++) {
+        const currentName = names[i];
+        const currentInitial = currentName[0];
+        initials += currentInitial;
+    }
+    return initials;
 }
 
-Return initials;
-)
+
+console.log(extractInitialsFromFullName("Abe Maxwell"));
+console.log(extractInitialsFromFullName("Martin Boxer Harris"));
+console.log(extractInitialsFromFullName("Felicity Rita Sarah Jones"));
+console.log(extractInitialsFromFullName("Mary Petunia"));
+
+
+
+// | Function call                                            | Expected output |
+// | -------------------------------------------------------- | --------------- |
+// | extractInitialsFromFullName("Abe Maxwell")               | "AM"            |
+// | extractInitialsFromFullName("Martin Boxer Harris")       | "MBH"           |
+// | extractInitialsFromFullName("Felicity Rita Sarah Jones") | "FRSJ"          |
+// | extractInitialsFromFullName("Mary Petunia")              | "MP"            |
