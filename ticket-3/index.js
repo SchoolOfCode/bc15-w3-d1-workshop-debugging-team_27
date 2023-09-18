e26a88205b17c4cb6927ab68b938111740281805
India Grant <60491066+indiagrant@users.noreply.github.com>
2023-09-18 16:17:54 +0100

    Ticket 3

--- ticket-3/index.js
+++ ticket-3/index.js
@@ -1,12 +1,18 @@
 function countNumberOfNinesInNumber(number) {
   let count = 0;
+  const numberStr = number.toString();
 
-  for (let i = 0; i < number.length; i++) {
-    const digit = number[i];
+  for (let i = 0; i < numberStr.length; i++) {
+    const digit = numberStr[i];
     if ("9" === digit) {
       count++;
     }
+    console.log(count);
   }
 
   return count;
 }
+
+
+let foo = countNumberOfNinesInNumber(999);
+console.log(foo);
