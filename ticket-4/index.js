4593ae038423a6ee9bedb62a2c076acef3fd2b22
Thomas Wardle <twardle6@googlemail.com>
2023-09-18 16:38:20 +0100

    Ticket 4 Done

--- ticket-4/index.js
+++ ticket-4/index.js
@@ -1,15 +1,25 @@
 function getGradeFromPoints(points) {
-  if ((points) => 90) {
+  if (points >= 90) {
     return "A";
-  } else if (Points >= 70) {
+  } else if (points >= 70) {
     return "B";
-  } else if (pointss >= 50) {
+  } else if (points >= 50) {
     return "C";
-  } else if (poInts >= 40) {
+  } else if (points >= 40) {
     return "D";
+  } else {
+    return "E";
   }
-  return "E";
 }
 
-const grade = getGradeFromPoints(90);
-console.log(Grade, "should be A");
+let grade = getGradeFromPoints(90);
+console.log(grade, "should be A");
+
+grade = getGradeFromPoints(70);
+console.log(grade, "should be B");
+
+grade = getGradeFromPoints(50);
+console.log(grade, "should be C");
+
+grade = getGradeFromPoints(40);
+console.log(grade, "should be D");
